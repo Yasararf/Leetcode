512057f774f46ef6daeb83bf32e9117e607329fa
Arafath <155822166+Yasararf@users.noreply.github.com>
2026-03-26 10:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (54.79%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            
+            if (digits[i] < 9) {
+                digits[i]++;   // add 1
+                return digits; // done
+            }
+            
+            digits[i] = 0; // carry
+        }
+
+        // if all digits were 9
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}

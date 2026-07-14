8f900bfcfd0035af07492eb1fc4c8a6c9a0577c3
Arafath <155822166+Yasararf@users.noreply.github.com>
2026-07-14 10:44:01 +0530

    Time: 1 ms (64.39%) | Memory: 43.2 MB (54.87%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,11 +1,9 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        String a = jewels;
-        String b = stones;
         int sum = 0;
-        for(int i = 0; i<a.length(); i++){
-            for(int j = 0; j<b.length(); j++){
-                if(a.charAt(i) == b.charAt(j)){
+        for(int i = 0; i<jewels.length(); i++){
+            for(int j = 0; j<stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)){
                     sum++;
                 }
             }

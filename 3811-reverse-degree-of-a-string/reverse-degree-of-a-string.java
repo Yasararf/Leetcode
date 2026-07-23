0537c622bb3e0d16eca0af04a5768a53604d9ef0
Arafath <155822166+Yasararf@users.noreply.github.com>
2026-07-23 20:20:20 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (49.52%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -4,14 +4,9 @@ class Solution {
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            // Calculate the 1-indexed position in the string
             int stringPosition = i + 1;
-            
-            // Calculate the value in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1)
+
             int reversedAlphabetValue = 26 - (c - 'a');
-            
-            // Add the product to the total sum
             totalSum += reversedAlphabetValue * stringPosition;
         }
         
